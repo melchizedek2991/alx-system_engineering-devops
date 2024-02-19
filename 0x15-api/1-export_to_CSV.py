a1ca81b211c3a6c0e94991055edb9fd90e5673e5
Ayodele Ayomidotun <ayodeleayomidotun@gmail.com>
2024-02-19 06:38:36 -0800

    All Done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,59 +1,24 @@
 #!/usr/bin/python3
-
-"""Exports to-do list information for a given employee ID to CSV format."""
-
-
-import csv
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
-
 import sys
 
 
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-if __name__ == "__main__":
+    response = requests.get(url)
+    username = response.json().get('username')
 
-    # Get the user ID from the command-line arguments provided to the script
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
 
-    user_id = sys.argv[1]
-
-
-    # Define the base URL for the JSON API
-
-    url = "https://jsonplaceholder.typicode.com/"
-
-
-    # Fetch user information from the API and
-
-    #   convert the response to a JSON object
-
-    user = requests.get(url + "users/{}".format(user_id)).json()
-
-
-    # Extract the username from the user data
-
-    username = user.get("username")
-
-
-    # Fetch the to-do list items associated with the
-
-    #   given user ID and convert the response to a JSON object
-
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-
-    # Use list comprehension to iterate over the to-do list items
-
-    # Write each item's details (user ID, username, completion status,
-
-    #   and title) as a row in the CSV file
-
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
-        [writer.writerow(
-
-            [user_id, username, t.get("completed"), t.get("title")]
-
-         ) for t in todos]
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
